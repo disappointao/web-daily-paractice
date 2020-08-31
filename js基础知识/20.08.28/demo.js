7a78fcaf556b8e55d2c9ca54cfa975820e9fa2e7
liuchuangao <liuchuangao@tenda.cn>
2020-08-31 17:54:05 +0800

    mergePromise

---
+++ js基础知识/20.08.28/demo.js
@@ -0,0 +1,39 @@
+const timeout = (ms) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+const ajax1 = () =>
+  timeout(2000).then(() => {
+    console.log("1");
+    return 1;
+  });
+const ajax2 = () =>
+  timeout(1000).then(() => {
+    console.log("2");
+    return 2;
+  });
+const ajax3 = () =>
+  timeout(2000).then(() => {
+    console.log("3");
+    return 3;
+  });
+const mergePromise = (ajaxArray) => {
+  // 1,2,3 done [1,2,3] 此处写代码 请写出ES6、ES3 2中解法
+  let arr = [];
+  return (async ()=>{
+    for(let key in ajaxArray){
+      let res = await ajaxArray[key]();
+      arr.push(res);
+    }
+    return new Promise(resolve => {
+      resolve(arr);
+    })
+  })()
+};
+mergePromise([ajax1, ajax2, ajax3]).then((data) => {
+  console.log("done");
+  console.log(data); // data 为[1,2,3]
+});
+

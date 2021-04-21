be0c9e2d96c9d87ee6d7d7c42294baa94699267f
liuchuangao <liuchuangao@tenda.cn>
2021-04-21 14:15:04 +0800

    [修改][lodash once方法]

--- 手撕js/Lodash/once.js
+++ 手撕js/Lodash/once.js
@@ -4,21 +4,22 @@ function sum(a,b){
     console.log('接收参数',a,b)
     return a + b
 }
-
 // const onceSum = _.once(sum)
 // onceSum(3,4)
 // onceSum(3,4)
 
 function myOnce(fn){
     let done
+    let result
     return function (){
         if(!done){
             done = true
-            return fn.apply(this,arguments)
+            result = fn.apply(this,arguments)
         }
+        return result
     }
 }
 
 const onceSum = myOnce(sum)
 onceSum(3,4)
-onceSum(3,4)
+console.log(onceSum(3, 4));

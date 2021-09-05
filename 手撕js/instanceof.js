6930a94358681b83cf94c353786ffbb067a3e34c
liuchuangao <1147608791@qq.com>
2021-09-06 00:03:53 +0800

    [新增][手撕js][instanceof实现]

--- 手撕js/instanceof.js
+++ 手撕js/instanceof.js
@@ -1,18 +1,49 @@
- function myInstanceof(target,source){
-    const proto = target.__proto__
-    if(!proto){
-        return false
-    }else{
-        if(proto === source.prototype){
-            return true
-        }
-        return myInstanceof(proto,source)
+ // function myInstanceof(target,source){
+ //    const proto = target.__proto__
+ //    if(!proto){
+ //        return false
+ //    }else{
+ //        if(proto === source.prototype){
+ //            return true
+ //        }
+ //        return myInstanceof(proto,source)
+ //    }
+ // }
+ // let a = {}
+ // console.log(myInstanceof(a,Object))
+ // function Person(){
+ //
+ // }
+ // let p = new Person()
+ // console.log(myInstanceof(p,Person))
+
+ // 递归方式
+ // function myInstanceOf(target, constructor) {
+ //  let proto = target.__proto__;
+ //  if(!proto) {
+ //    return false
+ //  }
+ //  if(proto === constructor.prototype){
+ //    return true
+ //  }
+ //  return myInstanceOf(proto, constructor)
+ // }
+
+ //while 循环遍历
+ function myInstanceOf(target, constructor) {
+  let proto = target.__proto__;
+  while (proto){
+    if(proto === constructor.prototype){
+      return true
     }
+    proto = proto.__proto__
+  }
+  return false
  }
  let a = {}
- console.log(myInstanceof(a,Object))
+ console.log(myInstanceOf(a,Array))
  function Person(){
 
  }
  let p = new Person()
- console.log(myInstanceof(p,Person))
+ console.log(myInstanceOf(p,Person))
